da9fe00d6ddfabe5febcb063c38b23524435ef2d
João Victorio <victoriojoao92@gmail.com>
2023-07-23 02:06:04 -0300

    hierarquia props

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -4,12 +4,12 @@
 
 // 7 essas props nada mais é do que um obj's que contém a lista de cada um desses atributos que eu defini na function App()
 // se eu ñ defini nenhum, ñ virá nenhum e se eu defini pelo menos um eu consigo acessar esse cara aqui que eu defini.
-
+import Titulo from "./Titulo"
 
 function Card(props){
   return (
     <div>
-      <h1>{props.titulo}</h1>
+      <Titulo titulo={props.titulo} />
       <h3>{props.subtitulo}</h3>
     </div>
   )
